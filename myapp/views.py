614bbaa71995f42aacea459bc46ebb06000ab652
Gulrez Alam <egulrezalam@gmail.com>
2023-11-07 02:16:07 +0530

    Delete unused files and code

--- myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def check(request):
-    return HttpResponse("<h1>Hello G1 && Najju, from my app!</h1>")
