e0df9585a6426d1c739e26ba0ef469ddabc4d9a3
Gulrez Alam <egulrezalam@gmail.com>
2023-11-07 00:16:42 +0530

    Updated settings.cpython-311.pyc file

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def check(request):
-    return HttpResponse("<h1>Hello, from my app!</h1>")
+    return HttpResponse("<h1>Hello G1 && Najju, from my app!</h1>")
